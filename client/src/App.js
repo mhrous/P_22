097dd54a67ae356bd0daa2beb6c1a6f09d4077df
mahrous <your_email_address@example.com>
2019-06-26 11:45:41 +0300

    start point server is ready and get from corouse rest api dising 3 and remove everything releate in test

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="App">hallwo world</div>;
+}
+
+export default App;
---
+++ server/src/config.js
@@ -0,0 +1,7 @@
+export default {
+  secrets: {
+    jwt: 'learneverything'
+  },
+  dbUrl: 'mongodb://localhost:27017/P_2',
+  port: 4000
+}
