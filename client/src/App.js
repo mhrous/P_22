865e73af0e1656c47237a733034be8caab9b9c6e
mahrous <your_email_address@example.com>
2019-06-26 12:17:47 +0300

    add some libary and add redux store is a defaclt but is a roteen work dont caer about it

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import { Provider } from "react-redux";
+import { Route, Redirect, Switch } from "react-router";
+import {
+  ConnectedRouter,
+  routerReducer,
+  routerMiddleware
+} from "react-router-redux";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+const reactRouterMiddleware = routerMiddleware(history);
+
+const store = createStore(
+  combineReducers({
+    router: routerReducer
+  }),
+  applyMiddleware(promiseMiddleware, reactRouterMiddleware)
+);
 
 function App() {
-  return <div className="App">hallwo world</div>;
+  return <Provider store={store}>hallw worald</Provider>;
 }
 
 export default App;
