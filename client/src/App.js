0e2d3b78f321e7daa4c335576405a797d2c56727
mahrous <your_email_address@example.com>
2019-06-26 12:45:04 +0300

    add router btween pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,39 @@ import React from "react";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise";
 import { Provider } from "react-redux";
-import { Route, Redirect, Switch } from "react-router";
-import {
-  ConnectedRouter,
-  routerReducer,
-  routerMiddleware
-} from "react-router-redux";
+import { Router, Route, Redirect, Switch } from "react-router";
 import { createBrowserHistory } from "history";
 
-const history = createBrowserHistory();
-const reactRouterMiddleware = routerMiddleware(history);
+import { routerReducer } from "react-router-redux";
 
 const store = createStore(
   combineReducers({
-    router: routerReducer
+    routing: routerReducer
   }),
-  applyMiddleware(promiseMiddleware, reactRouterMiddleware)
+  applyMiddleware(promiseMiddleware)
 );
 
-function App() {
-  return <Provider store={store}>hallw worald</Provider>;
-}
+const history = createBrowserHistory();
+
+const App = () => (
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/login" component={() => <div>log in page</div>} />
+        <Route exact path="/register" component={() => <div>room page</div>} />
+        <Route exact path="/lobby" component={() => <div>lobby page</div>} />
+        <Route
+          exact
+          path="/dashboard"
+          component={() => <div>dashboard page</div>}
+        />
+        <Route exact path="/game" component={() => <div>game page</div>} />
+        <Route exact path="/users" component={() => <div>all user page</div>} />
+        <Route exact path="/user/:id" component={() => <div>user page</div>} />
+        <Redirect to="/dashboard" />
+      </Switch>
+    </Router>
+  </Provider>
+);
 
 export default App;
